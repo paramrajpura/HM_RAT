7c07c21b86781328ee69203cb614c09b2cdfe1e2
EmanueleCiardo <91204500+EmanueleCiardo@users.noreply.github.com>
2022-07-20 21:34:20 +0200

    Update main_ica.m

--- main_ica.m
+++ main_ica.m
@@ -1,10 +1,13 @@
 %% independent component rejection
 
-
-% close all
-% clear all
+%% 1 - set up the stage
 
 
+close all
+clear all
+addpath('/home/genzel/Desktop/Emanuele')
+addpath('/home/genzel/Desktop/Emanuele/artifact_detector')
+addpath('/home/genzel/Desktop/Emanuele/artifact_detector/fieldtrip')
 cd /home/genzel/Desktop/Emanuele/processed_data/preproc % folder where the downsampled files are stored
 % ask the user to select the recording to load
 file = uigetfile("title","Select the study day to open"); 
@@ -14,60 +17,41 @@ load(file)
 recs=data_gen.recs;
 IC=data_gen.ic;
 varianza=data_gen.variance;
-chan=data_gen.chan;
+%chan=data_gen.chan;
 fs=600;
-%% 5 - plotting the original data to ask where there is an artifact
+comp=data_gen.comp;
+rat_num=str2num(data_gen.name(17));
+pfc=[59,3,0,57,86,0,93,94];
+hpc=[6,36,0,74,60,0,66,59];
+
+chan=[pfc(rat_num),hpc(rat_num)]; %rows that correspond to pfc and hpc, respectively
+%% 2 - plotting the original data to ask where there is an artifact
 
 close all
-plot1(recs,chan)
-sgtitle('Original recording')
-subplot(2,1,1)
-title('PFC')
-subplot(2,1,2)
-title('HPC')
-
-prompt = {'Select the location (in seconds) of an artifact'};
-dlgtitle = 'Input';
-dims = [1 35];
-definput = {'1','hsv'};
-art_time = str2num(cell2mat(inputdlg(prompt,dlgtitle,dims,definput)));
-
-%% plot information on the ICs
-
-close all
-           % Sampling frequency                    
-T = 1/fs;             % Sampling period       
-L = size(IC,2);             % Length of signal
-t = (0:L-1)*T;        % Time vector
-
-for i=1:4
-    j=2; %change to show other ICs
-    subplot(2,2,i)
-    show_IC=i+j;
-    x=fft(IC(show_IC,:));
-
-    P2 = abs(x/L);
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-
-    f = fs*(0:(L/2))/L;
-    plot(f,P1) 
-    title('IC nr.',i+j)
-
-    xlabel('Frequency')
-end
-% I would do so: take the fft of all the ICs, sum the values in the event
-% frequency bands and use it to determine how much of that frequency each
-% component is carrying
-%%
-gate=false;
-if gate
-    close all
-
-    spectrogram(IC(1,:),[],[],fs)
+gate=true;
+while gate
+    ttt=figure(1);
+    plot1(recs,chan)
+    sgtitle('Original recording - close to continue')
+    subplot(2,1,1)
+    title('PFC')
+    subplot(2,1,2)
+    title('HPC')
+    while ishandle(ttt)
+        pause(0.1)
+    end
+    prompt = {'Select the location (in seconds) of an artifact - -1 to show back the plot'};
+    dlgtitle = 'Input';
+    dims = [1 35];
+    definput = {'1','hsv'};
+    art_time = str2num(cell2mat(inputdlg(prompt,dlgtitle,dims,definput)));
+    if art_time >= 0
+        gate=false;
+    end
 end
 
-%%
+
+%% 3 - plot the ICs
 close all
 
 
@@ -77,7 +61,12 @@ close all
 %location of an artifact. Then I want to plot the single ICs with a
 %spectrogram, and only then I want to ask the user to select the ones to
 %remove
-%% 6 - Reject the components
+%% 4 - Reject the components
+close all
+data.name=data_gen.name;
+clear recs IC data_gen
+
+
 
 for i=1:2 
     cfg = [];
@@ -91,32 +80,9 @@ for i=1:2
         reconstr_original=reconstr;
     end
 end
-% close all
-% plot1(cleaned/mean(abs(cleaned(hpc,:))),hpc)
-% plot1(cleaned_not/mean(abs(cleaned_not(hpc,:))),hpc,'r')
-% % a=real(reconstr.trial{1});
-% % b=real(reconstr_original.trial{1});
-% % plot2(a/mean(a),b/mean(b),chan)
-% subplot(length(chan),1,1)
-% sgtitle('ICA-cleaned vs original signal')
-% title("PFC")
-% subplot(length(chan),1,2)
-% title("HPC")
-% legend("Original","zero","ICA-cleaned")
-% 
-% %recompose the data matrix, stacking all the trials together.
-% for i=1:size(reconstr.trial,2)
-%     cleaned(:,1+size(reconstr.trial{1},2)*(i-1):size(reconstr.trial{1},2)*(i))=reconstr.trial{i};
-%     timestamp(:,1+size(reconstr.trial{1},2)*(i-1):size(reconstr.trial{1},2)*(i))=reconstr.time{i}+(i)*2*reconstr.time{i}(end)-reconstr.time{i}(end-1);
-%     cleaned_not(:,1+size(reconstr_original.trial{1},2)*(i-1):size(reconstr_original.trial{1},2)*(i))=reconstr_original.trial{i};
-% end
-% 
-% s_clean=strcat(saved,'/processed_ICA');
-% s_nclean=strcat(saved,'/processed_original');
-% save(s_clean,'cleaned')
-% save(s_nclean,'cleaned_not')
 
-%% 7 - highpass components before removing them
+
+%% 5 - highpass components before removing them
 gate=false;
 if gate
     IC_to_remove=[];
@@ -127,48 +93,22 @@ if gate
         end
     end
 end
-%% remove outliers
-gate=false;
-if gate
-    j=1;
-    figure(1)
-    for i=0:1/10:10
-    outlier=abs(cleaned)>=std(cleaned)*i;
-    removed1(j)=sum(sum(outlier))/size(cleaned,1)/size(cleaned,2);
-    j=j+1;
-    end
-    plot([0:length(removed1)-1]/10,removed1*100,"LineWidth",2,"color","b")
-    title("percentage of outliers of original vs cleaned data")
-    ylabel("outliers, percentage")
-    xlabel("standard deviation")
 
-    j=1;
-    for i=0:1/10:10
-    outlier=abs(recs)>=std(recs)*i;
-    removed2(j)=sum(sum(outlier))/size(recs,1)/size(recs,2);
-    j=j+1;
-    end
-    hold on
-    plot([0:length(removed2)-1]/10,removed2*100,"LineWidth",2,"color","r")
-    title("percentage of outliers of original data")
-    ylabel("outliers, percentage")
-    xlabel("standard deviation")
-end
-
-%% save
+%% 6 - recompose the matrix and remove the unnecessary channels
 
 %recompose the data matrix, stacking all the trials together.
 for i=1:size(reconstr.trial,2)
-    cleaned(:,1+size(reconstr.trial{1},2)*(i-1):size(reconstr.trial{1},2)*(i))=reconstr.trial{i};
+    clean(:,1+size(reconstr.trial{1},2)*(i-1):size(reconstr.trial{1},2)*(i))=reconstr.trial{i}(chan,:);
     timestamp(:,1+size(reconstr.trial{1},2)*(i-1):size(reconstr.trial{1},2)*(i))=reconstr.time{i}+(i)*2*reconstr.time{i}(end)-reconstr.time{i}(end-1);
-    cleaned_not(:,1+size(reconstr_original.trial{1},2)*(i-1):size(reconstr_original.trial{1},2)*(i))=reconstr_original.trial{i};
+    nclean(:,1+size(reconstr_original.trial{1},2)*(i-1):size(reconstr_original.trial{1},2)*(i))=reconstr_original.trial{i}(chan,:);
 end
 
-data.cleaned=cleaned;
-data.original=cleaned_not;
+%% 7 - save the file back
+
+data.cleaned=clean;
+data.original=nclean;
 data.timestamp=timestamp;
-data.name=data_gen.name;
-data.chan=chan;
-save_folder=strcat('/home/genzel/Desktop/Emanuele/processed_data/ICA/',data_gen.name);
+%data.chan=chan_oi;
+save_folder=strcat('/home/genzel/Desktop/Emanuele/processed_data/ICA/',data.name);
 save(save_folder,'data','-v7.3')
 
