9b35a35414763eb212056a511606a53de7ef8c71
EmanueleCiardo <91204500+EmanueleCiardo@users.noreply.github.com>
2022-08-03 15:07:45 +0200

    Delete f.py

--- downsampling/f.py
+++
@@ -1,6 +0,0 @@
-list_dir=['/mnt/genzel/Rat/HM/Rat_HM_Ephys/mda_extracted/Rat_Hm_Ephys_Rat1_389236_20200904_homecageday.mountainsort',
-          '/mnt/genzel/Rat/HM/Rat_HM_Ephys/mda_extracted/Rat_Hm_Ephys_Rat2_389237_20200910_postsleep.mountainsort']
-for i in  list_dir:
-    directory = i
-    #directory = input("Enter the directory: ")
-    print(directory)
