f3ab9daa3d0b6c42db2cfa83b6a2ad4e958b5094
EmanueleCiardo <91204500+EmanueleCiardo@users.noreply.github.com>
2022-08-15 13:48:32 +0200

    Add files via upload

--- tablet.m
+++ tablet.m
@@ -22,13 +22,13 @@ file_names={
     'Rat_Hm_Ephys_Rat8_406579_20210821','Rat_Hm_Ephys_Rat8_406579_20210803','Rat_Hm_Ephys_Rat8_406579_20210810'
     };
 events={'ripple','spindle','delta'};
-events_p={'rippleXhr','spindleXhr','deltaXhr'};
+events_p={'rippleXhr','spindleXhr','deltaXr'};
 animals=[1,2,4,5,7,8];
-for focus=2:2
+for focus=1:1
     tab=table;
 
-    tab(1,:)=cell2table({'na','na','na','na','na','na','na'});
-    tab.Properties.VariableNames = ["Animal","StudyDay","Condition","Trial",strcat(events(focus),"_Count"),strcat(events(focus),"_Rate"),"NREM(min)"]
+    tab(1,:)=cell2table({'na','na','na','na','na','na','na','na','na'});
+    tab.Properties.VariableNames = ["Animal","StudyDay","Condition","Trial",strcat(events(focus),"_Count"),strcat(events(focus),"_Rate"),"NREM(min)","Thresh Amp","Thresh std"];
 
     for i=2:length(a)
         if strfind(a(i).name,'presleep')
@@ -37,7 +37,17 @@ for focus=2:2
             animal=str2num(a(i).name(17));
             studyday=data.name(end-16:end-9);
             sleep=sum(data.sleepscore==3)/60;
-            tab_temp=array2table({animal,studyday,condition,'presleep',size(data.(events{focus}),1),(size(data.(events{focus}),1))/sleep,sleep});
+            if focus==1
+                thr=data.thresholds(1)*data.std(2);
+                thr_std=data.thresholds(1);
+            elseif focus==2
+                thr=data.thresholds(2)*data.std(1);
+                thr_std=data.thresholds(1);
+            elseif focus==3
+                thr=data.thresholds(3)*data.std(1);
+                thr_std=data.thresholds(1);
+            end
+            tab_temp=array2table({animal,studyday,condition,'presleep',size(data.(events{focus}),1),round((size(data.(events{focus}),1))/sleep),round(sleep),round(thr),round(thr_std)});
             row=1+(find(animals==animal)-1)*15+(condition-1)*5;
             tab(row,:)=tab_temp;
         end
@@ -45,22 +55,33 @@ for focus=2:2
             load(a(i).name);
             [~,condition]=find(strcmp(file_names,data.name(1:end-10)));
             animal=str2num(a(i).name(17));
-            studyday=data.name(end-16:end-10);
+            studyday=data.name(end-17:end-10);
+            if focus==1
+                thr=data.thresholds(1)*data.std(2);
+                thr_std=data.thresholds(1);
+            elseif focus==2
+                thr=data.thresholds(2)*data.std(1);
+                thr_std=data.thresholds(1);
+            elseif focus==3
+                thr=data.thresholds(3)*data.std(1);
+                thr_std=data.thresholds(1);
+            end
+            
             sleep=sum(data.sleepscore(1:length(data.sleepscore)/4)==3)/60;
             row=2+(find(animals==animal)-1)*15+(condition-1)*5;
-            tab_temp=array2table({animal,studyday,condition,'postsleep_1',size(data.(events_p{focus}).('hour1'),2),size(data.(events_p{focus}).('hour1'),2)/sleep,sleep});
+            tab_temp=array2table({animal,studyday,condition,'postsleep_1',size(data.(events_p{focus}).('hour1'),2),round(size(data.(events_p{focus}).('hour1'),2)/sleep),round(sleep),round(thr),round(thr_std)});
             tab(row,:)=tab_temp;
             sleep=sum(data.sleepscore(1+length(data.sleepscore)/4:2*length(data.sleepscore)/4)==3)/60;
             row=3+(find(animals==animal)-1)*15+(condition-1)*5;
-            tab_temp=array2table({animal,studyday,condition,'postsleep_2',size(data.(events_p{focus}).('hour2'),2),size(data.(events_p{focus}).('hour2'),2)/sleep,sleep});
+            tab_temp=array2table({animal,studyday,condition,'postsleep_2',size(data.(events_p{focus}).('hour2'),2),round(size(data.(events_p{focus}).('hour2'),2)/sleep),round(sleep),round(thr),round(thr_std)});
             tab(row,:)=tab_temp;
             sleep=sum(data.sleepscore(1+2*length(data.sleepscore)/4:3*length(data.sleepscore)/4)==3)/60;
             row=4+(find(animals==animal)-1)*15+(condition-1)*5;
-            tab_temp=array2table({animal,studyday,condition,'postsleep_3',size(data.(events_p{focus}).('hour3'),2),size(data.(events_p{focus}).('hour3'),2)/sleep,sleep});
+            tab_temp=array2table({animal,studyday,condition,'postsleep_3',size(data.(events_p{focus}).('hour3'),2),round(size(data.(events_p{focus}).('hour3'),2)/sleep),round(sleep),round(thr),round(thr_std)});
             tab(row,:)=tab_temp;
             sleep=sum(data.sleepscore(1+3*length(data.sleepscore)/4:4*length(data.sleepscore)/4)==3)/60;
             row=5+(find(animals==animal)-1)*15+(condition-1)*5;
-            tab_temp=array2table({animal,studyday,condition,'postsleep_4',size(data.(events_p{focus}).('hour4'),2),size(data.(events_p{focus}).('hour4'),2)/sleep,sleep});
+            tab_temp=array2table({animal,studyday,condition,'postsleep_4',size(data.(events_p{focus}).('hour4'),2),round(size(data.(events_p{focus}).('hour4'),2)/sleep),round(sleep),round(thr),round(thr_std)});
             tab(row,:)=tab_temp;
         end
     end
